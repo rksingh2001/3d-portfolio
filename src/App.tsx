9ad7cf04fba3beb86eef2be0aa1eab8e8a8888c1
raunak <rksj2001@gmail.com>
2023-11-13 20:51:58 +0530

    fixed small issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-red-500 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
