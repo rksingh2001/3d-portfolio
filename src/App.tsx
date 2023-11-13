e81739ec9e6e9917319149d1ec1f7f78519bc96d
raunak <rksj2001@gmail.com>
2023-11-14 00:27:24 +0530

    added react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500 text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={'Home'} />
+          <Route path='/about' element={'About'} />
+          <Route path='/projects' element={'Projects'} />
+          <Route path='/contact' element={'Contact'} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
